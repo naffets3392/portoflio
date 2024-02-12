e5d53d78ba9fc4793ad58ac53bc48cfe289cd1ae
stefan <naffets@rocketmail.com>
2024-02-12 18:24:11 +0100

    test

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -8,26 +8,26 @@ import shopImage from '../../public/projectimages/shop.png'
 
 export const projects = [
     {
-        title: 'Trivia API Quizz',
-        description: 'My latest project. Here, I have used custom hooks and also the encapsulation method of function components. Here I also used TypeScript, Material-UI.',
+        title: "Trivia API Quizz",
+        description: "My latest project. Here, I have used custom hooks and also the encapsulation method of function components. Here I also used TypeScript, Material-UI.",
         github: "https://github.com/naffets3392/triviaquizz",
         image: quizzImage
     },
     {
-        title: 'Shop Project v2',
-        description: 'Here i have used Intersection Observer',
+        title: "Shop Project v2",
+        description: "Here i have used Intersection Observer",
         github: "https://github.com/naffets3392/shopprojectver2",
         image: shopv2Image
     },
     {
-        title: 'Shop Project',
+        title: "Shop Project",
         description: "I dont remember what I did here",
         github: "https://github.com/naffets3392/shopproject",
         image: shopImage
     },
     {
-        title: 'Memory Game Project',
-        description: 'One of my first projects',
+        title: "Memory Game Project",
+        description: "One of my first projects",
         github: "https://github.com/naffets3392/memorygame-project",
         image: memoryGameImage
     },
