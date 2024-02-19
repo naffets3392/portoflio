70d5200ff45b654ceaee3d939cb119d9fe65c11f
stefan <naffets@rocketmail.com>
2024-02-19 17:10:00 +0100

    changed text

--- src/components/homeComponent/HomeComponent.tsx
+++ src/components/homeComponent/HomeComponent.tsx
@@ -13,9 +13,10 @@ function HomeComponent() {
                 <Image src="/ZAVRSNAverzija.jpeg" fill alt='profile image' objectFit='cover' objectPosition='top'/>
             </div>
             <div className='text-slate-50 text-center'>
-                <h1 className='text-3xl mb-5'>Full-Stack Developer</h1>
-                <h5 className='flex-wrap'>My passion about coding started one year ago and I'm upgrading myself everyday with new knowledge.</h5>
-                <h5>I'm very outgoing, communicative and accustomed to teamwork.</h5>
+                <h1 className='text-3xl mb-5'>Front-End Developer</h1>
+                <h5 className='flex-wrap'>Well-versed in JavaScript, React, and Next.js, actively acquire new knowledge daily.</h5>
+                <h5>Currently delving into MongoDB and Mongoose, to broaden skill set for full-stack development.</h5>
+                <h5>Committed to continuous improvement, eager to contribute skills and enthusiasm to innovative projects, ready to add value to the teams I work with.</h5>
                 <div className='mt-5 flex gap-5 justify-center'>
                     <Link href='/skills'><button type="button" className="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Skills</button></Link>
                     <Link href='/projects'><button type="button" className="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Projects</button></Link>
